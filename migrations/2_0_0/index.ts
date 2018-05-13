0de9aa1d5bef2a8eb7c6e2cc167733bce5cd32b7
Tim Deschryver <timdeschryver@outlook.com>
2018-05-13 20:24:03 +0200

    release: 2.0.0-beta.2

--- migrations/2_0_0/index.ts
+++ migrations/2_0_0/index.ts
@@ -24,7 +24,7 @@ export default function(): Rule {
       const suffix = match(firstChar, '^') || match(firstChar, '~');
 
       // TODO: remove beta
-      pkg.dependencies['frontal'] = `${suffix}2.0.0-beta.1`;
+      pkg.dependencies['frontal'] = `${suffix}2.0.0-beta.2`;
       tree.overwrite(pkgPath, JSON.stringify(pkg, null, 2));
       context.addTask(new NodePackageInstallTask());
     }
